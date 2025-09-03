909fb0ed5a78a0de45ba907e75cbd1c47c66290f
Dunia <duniasan0@gmail.com>
2025-09-03 13:10:30 +0000

    Yes please implement let's proceed

--- src/locales/languages.ts
+++ src/locales/languages.ts
@@ -6,4 +6,9 @@ export const languages: { [key: string]: string } = {
   ja: '日本語',
   it: 'Italiano',
   pt: 'Português',
+  ar: 'العربية',
+  bn: 'বাংলা',
+  hi: 'हिन्दी',
+  tr: 'Türkçe',
+  ur: 'اردو',
 };
